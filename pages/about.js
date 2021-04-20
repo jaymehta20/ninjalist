953332aa16aee317d9b115499d19388d9d8d133e
Jay Mehta <jake2001007@gmail.com>
2021-04-20 19:45:43 +0530

    v1

---
+++ pages/about.js
@@ -0,0 +1,31 @@
+import Head from "next/head";
+
+const about = () => {
+  return (
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1>About Page</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur
+          corporis et quis, quo doloribus eum cupiditate voluptas illum modi
+          voluptate consequuntur itaque, non temporibus delectus vero, quidem
+          quam perferendis reiciendis alias? Laudantium voluptatum, debitis,
+          voluptatem assumenda provident reiciendis nemo cumque harum quae
+          quisquam ab facilis, esse quo. Eos, suscipit in laborum ut cupiditate
+          distinctio minus reiciendis. Excepturi, sint sunt optio esse pariatur
+          quibusdam velit facere, tenetur tempore deserunt omnis dolorum eius
+          mollitia. Distinctio, consequuntur vel? Suscipit, ab vel eos sunt
+          eligendi neque amet quod blanditiis recusandae illum harum fugit
+          consectetur at magnam sed vero sapiente sequi minima eius repellendus
+          culpa.
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default about;
---
+++ pages/ninjas/test.js
@@ -0,0 +1,14 @@
+const test = () => {
+  return (
+    <div>
+      <h1>Test Page</h1>
+      <p>
+        It's working really well, and i really like the simplicity it provides
+        and how easy is to create routes and use them without codig a single
+        line.
+      </p>
+    </div>
+  );
+};
+
+export default test;
